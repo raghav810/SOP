22fc67be2ab75cf678700b0659e6e849e2f03634
raghav810 <56407650+raghav810@users.noreply.github.com>
2021-07-29 13:50:57 +0530

    Add files via upload

---
+++ modp.py
@@ -0,0 +1,50 @@
+class ModP(int):
+    'Integers mod p, p a prime power.'
+    def __new__(cls, p, num):
+        self = int.__new__(cls, int(num) % p)
+        self.p = p
+        return self
+    
+    def __str__(self):
+        return "%d (mod %d)" % (self, self.p)
+    def __repr__(self):
+        return "%d %% %d" % (self, self.p)
+    
+    # arithmetic
+    def __neg__(self):
+        return ModP(self.p, self.p - int(self))
+    def __add__(self, other):
+        return ModP(self.p, int(self) + int(other))
+    def __radd__(self, other):
+        return ModP(self.p, int(other) + int(self))
+    def __sub__(self, other):
+        return ModP(self.p, int(self) - int(other))
+    def __rsub__(self, other):
+        return ModP(self.p, int(other) - int(self))
+    def __mul__(self, other):
+        return ModP(self.p, int(self) * int(other))
+    def __rmul__(self, other):
+        return ModP(self.p, int(other) * int(self))
+    def __div__(self, other):
+        if not isinstance(other, ModP):
+            other = ModP(self.p, other)
+        return self * other._inv()
+    def __rdiv__(self, other):
+        return other * self._inv()
+    
+    def _inv(self):
+        'Find multiplicative inverse of self in Z mod p.'
+        # extended Euclidean algorithm
+        rcurr = self.p
+        rnext = int(self)
+        tcurr = 0
+        tnext = 1
+        
+        while rnext:
+            q = rcurr // rnext
+            rcurr, rnext = rnext, rcurr - q * rnext
+            tcurr, tnext = tnext, tcurr - q * tnext
+        
+        if rcurr != 1:
+            raise ValueError("%d not a unit modulo %d" % (self, self.p))
+        return ModP(self.p, tcurr)
---
+++ poly.py
@@ -0,0 +1,80 @@
+# Polynomial class on Z or Z_p
+
+from collections import defaultdict
+
+class Poly:
+    'Polynomial class.'
+    def __init__(self, coeffs = None):
+        self.coeffs = defaultdict(int, isinstance(coeffs, int) and {0:coeffs} or coeffs or {})
+        self.deg = int(len(self.coeffs) and max(self.coeffs.keys()))
+    
+    def __call__(self, val):
+        'Evaluate polynomial for a given value.'
+        res = 0
+        for i in xrange(self.deg, -1, -1):
+            res = res * val + self.coeffs[i]
+        return res
+    
+    def __str__(self):
+        def term(coeff, expt):
+            if coeff == 1 and expt == 0:
+                return '1'
+            return ' * '.join(([] if coeff == 1 else [str(coeff)]) + \
+                              ([] if expt == 0 else ['X'] if expt == 1 else ['X ** %d' % expt]))
+            
+        return ' + '.join(term(self.coeffs[i], i) for i in self.coeffs if self.coeffs[i] != 0)
+    def __repr__(self):
+        return str(self)
+    
+    # arithmetic
+    def __neg__(self):
+        return Poly({(i, -self.coeffs[i]) for i in self.coeffs})
+    def __add__(self, other):
+        if not isinstance(other, Poly):
+            other = Poly(other)
+        res = Poly()
+        res.deg = max(self.deg, other.deg)
+        for i in xrange(res.deg+1):
+            res.coeffs[i] = self.coeffs[i] + other.coeffs[i]
+        return res
+    def __radd__(self, other):
+        if not isinstance(other, Poly):
+            other = Poly(other)
+        return other.__add__(self)
+    def __sub__(self, other):
+        if not isinstance(other, Poly):
+            other = Poly(other)
+        return self.__add__(other.__neg__())
+    def __rsub__(self, other):
+        if not isinstance(other, Poly):
+            other = Poly(other)
+        return other.__add__(self.__neg__())
+    
+    def __mul__(self, other):
+        if not isinstance(other, Poly):
+            other = Poly(other)
+        res = Poly()
+        res.deg = self.deg + other.deg # consider case where other is 0
+        for i in xrange(res.deg+1):
+            for j in xrange(i+1):
+                res.coeffs[i] += self.coeffs[j] * other.coeffs[i - j]
+        return res
+    def __rmul__(self, other):
+        if not isinstance(other, Poly):
+            other = Poly(other)
+        return other.__mul__(self)
+        
+    def __pow__(self, other):
+        if not isinstance(other, int) or other < 0:
+            raise ValueError("Exponent %d is not a natural number" % other)
+        res = Poly(1)
+        while other:
+            res *= self
+            other -= 1
+        return res
+
+X = Poly({1:1})
+
+def derivative(p):
+    'Return derivative of polynomial.'
+    return Poly({(i - 1, i * p.coeffs[i]) for i in p.coeffs if i != 0})
